3d69de1af3784c095c10d1e1a60150f9eb126fe6
cokcok <godvillager@gmail.com>
2022-05-31 22:38:31 +0800

    First commigt

---
+++ src/components/Cards.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div className='w-full py-[10rem] px-4 bg-white'>
+        <div className='max-w-[1240px]'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Cards
